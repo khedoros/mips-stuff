797eff4a3fd5c45ba79cdc8122ab48ffcbe908ef
khedoros <khedoros@gmail.com>
2025-03-22 23:30:13 -0700

    Bit of CPU rearrangement

--- cpu.cpp
+++ cpu.cpp
@@ -8,53 +8,8 @@
 #include "instruction.hpp"
 #include "disassembly.hpp"
 
-cpu::cycleCount cpu::executeBranch(uint32_t inst) {
-    // branch type: inst_t::getRt(inst)
-    // offset register: inst_t::getRs(inst)
-    // 16-bit immediate: inst_t::getImm16(inst);
-    return 0;
-}
+cpu::cpu() : cycle{0}, pc{0xbfc00000} {
 
-cpu::cycleCount cpu::executeAlu(uint32_t inst) {
-    switch(inst_t::getFunc(inst)) {
-        case 0: case 2: case 3: // SLL SRL SRA
-            if(inst == 0) return 0; // "nop";
-            // function: inst_t::getFunc(inst)
-            // destination: inst_t::getRd(inst)
-            // register operator: inst_t::getRt(inst)
-            // rotation constant: inst_t::getImm5(inst)
-            return 0;
-        case 4: case 6: case 7: // SLLV SRLV SRAV
-            return 0;
-            // function: inst_t::getFunc(inst)
-            // destination: inst_t::getRd(inst)
-            // register operator: inst_t::getRt(inst)
-            // rotation register: inst_t::getRs(inst);
-        case 8: // JR
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)];
-        case 9: // JALR
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)] + csp + regName[inst_t::getRs(inst)];
-        case 12: case 13:
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + std::to_string(inst>>6); // top 6 bits are 0, next 20 bits are a "comment" value
-        case 16: case 18:
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)];
-        case 17: case 19:
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)];
-        case 24: case 25: case 26: case 27:
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)] + csp + regName[inst_t::getRt(inst)];
-        case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 42: case 43:
-            return 0;
-            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)] + csp + regName[inst_t::getRs(inst)] + csp + regName[inst_t::getRt(inst)];
-        default:
-            return 0;
-            // unrecognized/invalid
-    }
 }
 
 cpu::cycleCount cpu::execute(uint32_t inst) {
@@ -127,4 +82,53 @@ cpu::cycleCount cpu::execute(uint32_t inst) {
             return 0;
             // Unrecognized/invalid
     }
+}
+
+cpu::cycleCount cpu::executeAlu(uint32_t inst) {
+    switch(inst_t::getFunc(inst)) {
+        case 0: case 2: case 3: // SLL SRL SRA
+            if(inst == 0) return 0; // "nop";
+            // function: inst_t::getFunc(inst)
+            // destination: inst_t::getRd(inst)
+            // register operator: inst_t::getRt(inst)
+            // rotation constant: inst_t::getImm5(inst)
+            return 0;
+        case 4: case 6: case 7: // SLLV SRLV SRAV
+            return 0;
+            // function: inst_t::getFunc(inst)
+            // destination: inst_t::getRd(inst)
+            // register operator: inst_t::getRt(inst)
+            // rotation register: inst_t::getRs(inst);
+        case 8: // JR
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)];
+        case 9: // JALR
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)] + csp + regName[inst_t::getRs(inst)];
+        case 12: case 13:
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + std::to_string(inst>>6); // top 6 bits are 0, next 20 bits are a "comment" value
+        case 16: case 18:
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)];
+        case 17: case 19:
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)];
+        case 24: case 25: case 26: case 27:
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRs(inst)] + csp + regName[inst_t::getRt(inst)];
+        case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 42: case 43:
+            return 0;
+            // return func[inst_t::getFunc(inst)] + sp + regName[inst_t::getRd(inst)] + csp + regName[inst_t::getRs(inst)] + csp + regName[inst_t::getRt(inst)];
+        default:
+            return 0;
+            // unrecognized/invalid
+    }
+}
+
+cpu::cycleCount cpu::executeBranch(uint32_t inst) {
+    // branch type: inst_t::getRt(inst)
+    // offset register: inst_t::getRs(inst)
+    // 16-bit immediate: inst_t::getImm16(inst);
+    return 0;
 }
--- cpu.hpp
+++ cpu.hpp
@@ -5,6 +5,7 @@
 class cpu {
 private:
     typedef uint64_t cycleCount;
+    cycleCount cycle;
     uint32_t reg[32];
     uint32_t pc;
     uint32_t hi,lo;
@@ -13,6 +14,6 @@ private:
     cycleCount executeAlu(uint32_t inst);
 
 public:
-
+    cpu();
     cycleCount execute(uint32_t inst);
 };
